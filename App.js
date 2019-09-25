0af523bc85343f6cd5cd19e60570a05d930f2614
Abdelhak <abdelhak.belaid@outlook.com>
2019-09-25 10:21:20 +0200

    [FEAT] expenses screen

--- App.js
+++ App.js
@@ -4,14 +4,18 @@ import LoginScreen from './components/login/LoginScreen';
 import SignupScreen from './components/signup/SignupScreen';
 import MainScreen from './components/MainScreen';
 import LoadingScreen from './components/loading/LoadingScreen';
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+import ExpensesScreen from './components/expenses/ExpensesScreen';
+import MainHeader from './components/MainHeader';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
 const MainNavigator = createStackNavigator({
-    Loading: { screen: LoadingScreen},
-    Login: {screen: LoginScreen},
-    Signup: {screen: SignupScreen},
-    Main: { screen: MainScreen }
+    // Loading: { screen: LoadingScreen },
+    // Login: { screen: LoginScreen },
+    // Signup: { screen: SignupScreen },
+    Expenses: { screen: ExpensesScreen },
+    // Main: { screen: MainScreen }
+    // MainHeader: { screen: MainHeader }
 });
 
 const App = createAppContainer(MainNavigator);
